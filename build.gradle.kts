84e98309e5433a8188529777229c6a674f34810d
Ayushkiller <malikayush999@gmail.com>
2024-04-09 20:20:04 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,24 +12,18 @@ repositories {
 dependencies {
     val json = JsonSlurper().parseText(file("src/main/resources/plugin.json").readText()) as Map<*, *>
     project.version = json["version"]!!
-
     val mindustryVersion = json["minGameVersion"]
-    val usefulHash = "2f1d9eb899"
-
     compileOnly("com.github.anuken.arc:arc-core:v$mindustryVersion")
     compileOnly("com.github.anuken.mindustry:core:v$mindustryVersion") {
         exclude(group = "com.github.Anuken.Arc")
     }
-
     compileOnly("com.github.anuken.mindustry:server:v$mindustryVersion") {
         exclude(group = "com.github.Anuken.Arc")
     }
-
     implementation("dev.morphia.morphia:morphia-core:2.3.8")
-
     implementation("com.discord4j:discord4j-core:3.2.6")
     runtimeOnly("io.netty:netty-transport-native-epoll::linux-aarch_64")
-  implementation ("net.time4j:time4j-base:5.9.1")
+    implementation ("net.time4j:time4j-base:5.9.1")
     implementation("org.jline:jline-reader:3.21.0")
     implementation("org.jline:jline-console:3.21.0")
     implementation("org.jline:jline-terminal-jna:3.21.0")
