2c06010547d252f83626b243c98f2617a1747e47
Ayushkiller <malikayush999@gmail.com>
2024-04-09 16:10:57 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,14 @@ dependencies {
     val usefulHash = "2f1d9eb899"
 
     compileOnly("com.github.anuken.arc:arc-core:v$mindustryVersion")
-    compileOnly("com.github.anuken.mindustry:core:v$mindustryVersion") 
-    compileOnly("com.github.anuken.mindustry:server:v$mindustryVersion") 
+    compileOnly("com.github.anuken.mindustry:core:v$mindustryVersion") {
+        exclude(group = "com.github.Anuken.Arc")
+    }
+
+    compileOnly("com.github.anuken.mindustry:server:v$mindustryVersion") {
+        exclude(group = "com.github.Anuken.Arc")
+    }
+
     implementation("com.github.osp54:Sock:ed404a96ff")
     implementation("dev.morphia.morphia:morphia-core:2.3.8")
 
